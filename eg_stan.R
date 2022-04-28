9864f2d28fbe4a1fc30a44e0a74bbbf74a6576f2
Stephen David Gregory <stephendavidgregory@gmail.com>
2022-04-28 11:02:37 +0100

    Switched from rstan to cmdstanr that seems to be more stable.

--- eg_stan.R
+++ eg_stan.R
@@ -1,13 +1,12 @@
-library(rstan)
-# see ?stan
-scode <- "
-parameters {
-  real y[2]; 
-} 
-model {
-  y[1] ~ normal(0, 1);
-  y[2] ~ double_exponential(0, 2);
-} 
-"
-fit1 <- stan(model_code = scode, iter = 10, verbose = FALSE) 
-print(fit1)
+library(cmdstanr)
+file <- file.path(cmdstan_path(), "examples", "bernoulli", "bernoulli.stan")
+mod <- cmdstan_model(file)
+data_list <- list(N = 10, y = c(0,1,0,0,0,0,0,0,0,1))
+fit <- mod$sample(
+  data = data_list, 
+  seed = 123, 
+  chains = 4, 
+  parallel_chains = 4,
+  refresh = 500 # print update every 500 iters
+)
+print(fit)
