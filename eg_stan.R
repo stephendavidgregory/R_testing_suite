0af3413fbb526582ba5c590375b2beae55ba9c4b
Stephen David Gregory <stephendavidgregory@gmail.com>
2022-04-28 11:27:36 +0100

    Force stan to compile exe.

--- eg_stan.R
+++ eg_stan.R
@@ -1,4 +1,7 @@
 library(cmdstanr)
+if(file.exists(file.path(cmdstan_path(), "examples", "bernoulli", "bernoulli.exe"))) {
+  unlink(file.path(cmdstan_path(), "examples", "bernoulli", "bernoulli.exe"))
+}
 file <- file.path(cmdstan_path(), "examples", "bernoulli", "bernoulli.stan")
 mod <- cmdstan_model(file)
 data_list <- list(N = 10, y = c(0,1,0,0,0,0,0,0,0,1))
